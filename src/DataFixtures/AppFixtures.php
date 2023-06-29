45785bb41e56dd7a4e24f0e69fb07224574e1b9e
catherine <cat@MacBook-Air-de-catherine.local>
2023-06-29 16:02:39 +0200

    add user and entityListener for password hashing

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,11 +3,14 @@
 namespace App\DataFixtures;
 
 use App\Entity\Recipe;
+use App\Entity\User;
 use Faker\Factory;
 use Faker\Generator;
 use App\Entity\Ingredient;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 
 class AppFixtures extends Fixture
@@ -17,9 +20,12 @@ class AppFixtures extends Fixture
      */
      private Generator $faker;
 
-     public function __construct()
+     private UserPasswordHasherInterface $hasher;
+
+     public function __construct(UserPasswordHasherInterface $hasher)
      {
          $this->faker = Factory::create('fr_FR');
+         $this->hasher = $hasher;
      }
 
     public function load(ObjectManager $manager): void
@@ -50,8 +56,20 @@ class AppFixtures extends Fixture
                 $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
             }
             $manager->persist($recipe);
-
         }
+
+        //for users
+        for($u = 0; $u <10; $u++){
+            $user = new User();
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(["ROLE_USER"])
+                ->setPlainPassword("password");
+
+            $manager->persist($user);
+        }
+
         $manager->flush();
     }
 }
