02c6ba1a291d57712d245ffe064ce6717cdb49fc
catherine <cat@MacBook-Air-de-catherine.local>
2023-07-10 13:24:07 +0200

    adding home service mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,19 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Service\MailService;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailService $mailService): Response
     {
         $contact = new Contact();
         if($this->getUser()){
@@ -30,25 +28,13 @@ class ContactController extends AbstractController
             $manager->persist($contact);
             $manager->flush();
 
-
-            $email = (new TemplatedEmail())
-                ->from($contact->getEmail())
-                ->to('admin@symrecipe.com')
-                ->subject($contact->getSubject())
-                // path of the Twig template to render
-                ->htmlTemplate('emails/contact.html.twig')
-
-                // pass variables (name => value) to the template
-                ->context([
-                    'expiration_date' => new \DateTime('+7 days'),
-                    'contact' => $contact,
-                ]);
-
-            try{
-                $mailer->send($email);
-            }catch (TransportExceptionInterface $e) {
-                dd($e);
-            }
+            //Email
+            $mailService->sendEmail(
+                $contact->getEmail(),
+                $contact->getSubject(),
+                'emails/contact.html.twig',
+                ['contact' => $contact]
+            );
 
             $this->addFlash(
                 'success',
