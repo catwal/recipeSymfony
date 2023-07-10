7c0982e087890af93d903f52a0c34551caabf6db
catherine <cat@MacBook-Air-de-catherine.local>
2023-07-10 12:35:42 +0200

    adding mail service

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,17 +4,19 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use ContainerTtEf5f4\getContactRepositoryService;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         if($this->getUser()){
@@ -28,12 +30,30 @@ class ContactController extends AbstractController
             $manager->persist($contact);
             $manager->flush();
 
+
+            $email = (new TemplatedEmail())
+                ->from($contact->getEmail())
+                ->to('admin@symrecipe.com')
+                ->subject($contact->getSubject())
+                // path of the Twig template to render
+                ->htmlTemplate('emails/contact.html.twig')
+
+                // pass variables (name => value) to the template
+                ->context([
+                    'expiration_date' => new \DateTime('+7 days'),
+                    'contact' => $contact,
+                ]);
+
+            try{
+                $mailer->send($email);
+            }catch (TransportExceptionInterface $e) {
+                dd($e);
+            }
+
             $this->addFlash(
                 'success',
                 'votre demande a bien été envoyé'
             );
-
-            return $this->redirectToRoute('home.index');
         }
         return $this->render('pages/contact/index.html.twig', [
             'form' => $form->createView(),
